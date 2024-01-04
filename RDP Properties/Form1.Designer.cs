f3edf4c748ae5a996b062a32de678f2fdd3a9f51
DHARSHAN MUTHUKUMAR <muthukumardharshan50@gmail.com>
2024-01-04 11:10:23 +0530

    Add files via upload

---
+++ RDP Properties/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace RDP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btn_connect);
+            this.panel1.Controls.Add(this.txtPhoneNumber);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(788, 363);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(666, 199);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.BackColor = System.Drawing.Color.Red;
+            this.btn_connect.ForeColor = System.Drawing.Color.Black;
+            this.btn_connect.Location = new System.Drawing.Point(246, 118);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(150, 42);
+            this.btn_connect.TabIndex = 2;
+            this.btn_connect.Text = "Submit";
+            this.btn_connect.UseVisualStyleBackColor = false;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(114, 52);
+            this.txtPhoneNumber.Multiline = true;
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(423, 38);
+            this.txtPhoneNumber.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label1.Location = new System.Drawing.Point(264, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phone Number";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2301, 965);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "EG Exam";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RDP Properties/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+using AxMSTSCLib;
+
+namespace RDP
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        private AxMsRdpClient9NotSafeForScripting rdpClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //InitializeRDP();
+        }
+
+        private void InitializeRDP()
+        {
+            rdpClient = new AxMsRdpClient9NotSafeForScripting();
+            rdpClient.Dock = DockStyle.Fill;
+            this.Controls.Add(rdpClient);
+
+            // Set other RDP client properties as needed
+            rdpClient.Server = "74.235.195.24";
+            rdpClient.UserName = "dharshan";
+            rdpClient.AdvancedSettings9.ClearTextPassword = "@Dharshan007";
+
+            // Optionally, set other RDP client properties here...
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtPhoneNumber.Visible = false;
+                btn_connect.Visible = false;
+                panel1.Visible = false;
+
+                // Connect to VM
+                InitializeRDP();
+                rdpClient.Connect();
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+            }
+        }
+    }
+}
